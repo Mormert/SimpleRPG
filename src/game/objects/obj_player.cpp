5c9ee195df0ae411595e8d965594d4e9b2a36e43
Mormert <github@mormert.com>
2020-05-20 22:13:49 +0200

    Added camera component

--- src/game/objects/obj_player.cpp
+++ src/game/objects/obj_player.cpp
@@ -4,6 +4,7 @@
 #include "sprite_renderer.hpp"
 #include "sprite.hpp"
 #include "comp_player_controller.hpp"
+#include "comp_cam_transform.hpp"
 
 namespace rpg::objects
 {
@@ -18,16 +19,21 @@ namespace rpg::objects
 
         components::Transform *transform = new components::Transform(x, y);
         components::SpriteRenderer *spriteRenderer = new components::SpriteRenderer(transform);
+        components::CamTransformController *camController = new components::CamTransformController(transform, 5.0f);
         components::TopDownController *topDownController = new components::TopDownController(transform);
         components::PlayerController *player = new components::PlayerController(topDownController);
         player->SetMoveSpeed(moveSpeed);
 
         Sprite *sprite = new rpg::Sprite{
             rpg::objects::SpriteSheet::GetTexture(texturePath.c_str()), spriteX, spriteY, spriteWidth, spriteHeight};
+        sprite->SetOrigin(Vector2{spriteWidth / 2.0f, spriteHeight / 2.0f});
         spriteRenderer->AddSprite(sprite);
 
+        // Note that the order of which the componets are added have an
+        // impact on the which order of the components update method is called
         AddComponent(transform);
         AddComponent(spriteRenderer);
+        AddComponent(camController);
         AddComponent(topDownController);
         AddComponent(player);
     }
